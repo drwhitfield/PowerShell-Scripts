f2ef7790a56dd8f24123342a0abb9f4d7b683406
drwhitfield <43507455+drwhitfield@users.noreply.github.com>
2022-12-06 13:56:47 -0800

    Update DeviceInfo.ps1

--- DeviceInfo.ps1
+++ DeviceInfo.ps1
@@ -2,22 +2,17 @@
 .SYNOPSIS
     The script defines a class and each object within the class is defined/initialized by PowerShell commandlets. Consider using constructor in future,
     but this is not necessary.
-
 .DESCRIPTION
     A PowerShell script that collects useful information from a compromised or potentially compromised JCI computer asset running Windows
-
 .LINK
     Reference GitHub Repository here
-
 .NOTES
     Version:    1.0
     Author:     Donald Whitfield
     Contact:    donald.r.whitfield@jci.com
     Created:    12-04-2022
     Purpose:    Automate collection of data necessary for IR investigations
-
 #>
-
 
 class Device{
 
@@ -43,12 +38,26 @@ $device.Processes = Get-Process * | Out-String
 $device.Connections = Get-NetTCPConnection | Where-Object state -ne Bound | Out-String
 $device.AutoRunConf = Get-CimInstance Win32_StartupCommand | Select-Object Name, Command, Location, User | Out-String
 
+function PrintDeviceInfo
+{
 
-Write-Host -NoNewLine "System Hostname: " -ForegroundColor Green; $device.Hostname
-Write-Host -NoNewLine "`nDetected Drives: " -ForegroundColor Green; $device.Drives
-Write-Host -NoNewLine "`nUser Accounts Identified: " -ForegroundColor Green; $device.Accounts
-Write-Host -NoNewLine "`nScheduled Tasks Configured: " -ForegroundColor Green; $device.SchedTasks
-Write-Host -NoNewLine "`nLog of Connected USB Devices: " -ForegroundColor Green; $device.USB_History
-Write-Host -NoNewLine "`nSystem Processes Listing: " -ForegroundColor Green; $device.Processes
-Write-Host -NoNewLine "`nDevice Network Connections: " -ForegroundColor Green; $device.Connections
-Write-Host -NoNewLine "`nDisplay AutoRun Configuration: " -ForegroundColor Green; $device.AutoRunConf
+$device_objs = @(
+    $device.Hostname, $device.Drives, $device.Accounts, $device.SchedTasks, $device.USB_History,
+    $device.Processes, $device.Connections, $device.AutoRunConf)
+
+$device_info = @(
+    "System Hostname: ", "`nDetected Drives: ", "`nUser Accounts Identified: ", "`nScheduled Tasks Configured: ",
+    "`nLog of Connected USB Devices: ", "`nSystem Processes Listing: ", "`nDevice Network Connections: ",
+    "`nDisplay AutoRun Configuration: ")
+
+$total = $device_info.Count
+
+for ($i=0; $i -le $total; $i++)
+    {
+        Write-Host -NoNewline $device_info[$i] -ForegroundColor Green; $device_objs[$i]
+    }
+
+}
+
+PrintDeviceInfo
+
